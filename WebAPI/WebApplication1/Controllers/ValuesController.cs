cb6cb72aaee4e08a584b8a62a6bd2537655d9bdb
Blaclix92 <bennymohan08@gmail.com>
2018-02-14 13:52:56 +0100

    Add new WebAPI and add swagger UI to both webapis

---
+++ WebAPI/WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok(new Value { Id = id, Text = "value" + id });
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody]Value value)
+        {
+            if (!ModelState.IsValid)
+            {
+
+                return BadRequest(ModelState);
+            }
+            return CreatedAtAction("Get", new { id = value.Id }, value);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+}
--- WebAPI/WebApplication1/Startup.cs
+++ WebAPI/WebApplication1/Startup.cs
@@ -4,30 +4,29 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-using WebAPI.Data;
 using Microsoft.Extensions.Configuration;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Swashbuckle.AspNetCore.Swagger;
 
-namespace WebAPI
+namespace WebApplication1
 {
     public class Startup
     {
-        public IConfiguration Configuration { get; }
-
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<TodoContext>(options => options.UseInMemoryDatabase("foo"));
+            //services.AddDbContext<TodoContext>(options => options.UseInMemoryDatabase("foo"));
             services.AddMvc();
-            services.AddSwaggerGen(c => {
+            services.AddSwaggerGen( c => {
                 c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
             });
         }
@@ -39,16 +38,13 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
-          
+            
+
             app.UseMvc();
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-            });
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("MVC didnt find anything");
             });
         }
     }
